6c009748f18181ea23912b1b63a901aa654d790a
kisslaay <chandrakislay@gmail.com>
2020-03-28 12:14:23 +0530

    login and folder structure

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>HOLD MY DOCS</h1>
+        <p>A simple document managing app</p>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
