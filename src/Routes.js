404378739ffc8ceb5be6e6c5ea7c2826b3ef04ed
kisslaay <chandrakislay@gmail.com>
2020-03-30 17:53:54 +0530

    signup page

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import Home from "./containers/Home";
 
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/login" exact component={Login} />
+      <Route path="/signup" exact component={Signup} />
 
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
