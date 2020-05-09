c27aa7d489127a582455c95e781e66cd6899db82
kisslaay <chandrakislay@gmail.com>
2020-05-10 00:58:29 +0530

    change requested #1

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,7 +6,7 @@ import Credentials from "../data/credentials.json";
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  var userFound = "no";
+  let userFound = false;
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -18,12 +18,12 @@ export default function Login(props) {
         Credentials.map((credentialData, index) => {
           if (credentialData.email === email && credentialData.password === password) {
             event.preventDefault();
-            userFound = "yes";
+            userFound = true;
             alert("Logged in");
           }
           return null;
         })
-        if (userFound === "no") {
+        if (!userFound) {
           alert("Wrong email or password");
         }
       } else {
